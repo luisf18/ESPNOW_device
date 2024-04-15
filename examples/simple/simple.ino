895c9385dea2a7f2bf23981f41b77e497ad81468
luisf18 <luisfelipefonte@gmai.com>
2024-04-15 02:25:59 -0300

    exemplo simple.ino

--- examples/simple/simple.ino
+++ examples/simple/simple.ino
@@ -1,5 +1,6 @@
 #include "ESPNOW_device.h"
 
+// comment to upload to client
 //#define SERVER
 
 void espnow_handle(espnow_device_event_t ev, int id){
@@ -15,15 +16,11 @@ void espnow_handle(espnow_device_event_t ev, int id){
     Serial.printf( "[ESPNOW DEVICE EVT CONNECTED %d]\n", id );
   }else if( ev == ESPNOW_EVT_RECIVE ){
     Serial.printf( "[ESPNOW DEVICE EVT RECIVE %d]\n", id );
-    //Serial.printf( " MAC: %s\n", mac2str( ESPNOW_device.connections[id].remote_mac ).c_str() );
-    //Serial.printf( " Name: %s\n", ESPNOW_device.connections[id].frame_in.name );
-    //Serial.printf( " Name RX: %s\n", ESPNOW_device.connections[id].frame_in.name_rx );
-    //Serial.printf( " load.len: %d\n", ESPNOW_device.connections[id].frame_in.len );
-    //Serial.printf( " load: %d\n", *( (int*) ESPNOW_device.connections[id].frame_in.body ) );
   }
 }
 
 void setup() {
+
   // inicia o LED
   pinMode(2,OUTPUT);
   digitalWrite(2,LOW);
@@ -48,10 +45,5 @@ void setup() {
 }
 
 void loop() {
-  if( Serial.available() > 0 ){
-    String cmd = Serial.readString();
-    Serial.println(cmd);
-    Serial.printf( "espnow_device_connection_counter %d", ESPNOW_device.connections_counter );
-  }
   ESPNOW_device.update();
 }
